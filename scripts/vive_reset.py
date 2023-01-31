ded8ae303561346816f85ecf90b14e3e5d70be09
hideki.okamoto <hideki.okamoto@toyota.com>
2023-01-31 14:55:58 -0500

    upload

--- scripts/vive_reset.py
+++ scripts/vive_reset.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python3
 import roslib
 import rospy
 import tf
@@ -7,20 +7,20 @@ import tf
 # This node adjusts the vive_world frame, such that all child frames are
 # shifted so that the tracked device is at the origin.
 # i.e. Everything moves so that Vive Tracker is moved to 0,0,0
-if __name__ == '__main__':
-    rospy.init_node('vive_tracker_reset')
+if __name__ == "__main__":
+    rospy.init_node("vive_tracker_reset")
 
     listener = tf.TransformListener()
 
     tf_count = 0
-    zero_tf = listener.lookupTransform('vive_world', 'vive_world', rospy.Time(0))
-    br = tf.TransformBroadcaster() 
+    zero_tf = listener.lookupTransform("vive_world", "vive_world", rospy.Time(0))
+    br = tf.TransformBroadcaster()
     topic_names = rospy.get_published_topics("/vive/")
     tracker_name = ""
     # Get the first tracked device that isn't a reference frame.
     for name in topic_names:
         if "LHB" not in name[0]:
-            tracker_name = name[0].replace('/vive/','').replace('_odom','')
+            tracker_name = name[0].replace("/vive/", "").replace("_odom", "")
             print(tracker_name)
             break
     rate = rospy.Rate(1.0)
@@ -28,17 +28,26 @@ if __name__ == '__main__':
         if tracker_name is "":
             continue
         try:
-            (trans,rot) = listener.lookupTransform('vive_world', tracker_name, rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            (trans, rot) = listener.lookupTransform(
+                "vive_world", tracker_name, rospy.Time(0)
+            )
+        except (
+            tf.LookupException,
+            tf.ConnectivityException,
+            tf.ExtrapolationException,
+        ):
             continue
-        
- 
+
         if tf_count < 10:
-            (trans,rot) = listener.lookupTransform('vive_world', tracker_name, rospy.Time(0))
-            br.sendTransform(trans, rot, rospy.Time.now(),'zero_location','vive_world')
+            (trans, rot) = listener.lookupTransform(
+                "vive_world", tracker_name, rospy.Time(0)
+            )
+            br.sendTransform(
+                trans, rot, rospy.Time.now(), "zero_location", "vive_world"
+            )
             inv_trans = trans[:]
             inv_rot = rot[:]
-            #rospy.loginfo('Inverse transform found.')
+            # rospy.loginfo('Inverse transform found.')
             tf_count += 1
         rate.sleep()
         break
@@ -50,7 +59,13 @@ if __name__ == '__main__':
     inv_rot[2] = -rot[2]
     inv_rot[3] = -rot[3]
 
-    br.sendTransform(inv_trans, [0,0,0,1], rospy.Time.now(),'vive_world','vive_world_orientation')
-    while  not rospy.is_shutdown():
-        br.sendTransform(trans, rot, rospy.Time.now(),'zero_location','vive_world')
-    #rospy.signal_shutdown("Tracker reset to origin.")
+    br.sendTransform(
+        inv_trans,
+        [0, 0, 0, 1],
+        rospy.Time.now(),
+        "vive_world",
+        "vive_world_orientation",
+    )
+    while not rospy.is_shutdown():
+        br.sendTransform(trans, rot, rospy.Time.now(), "zero_location", "vive_world")
+    # rospy.signal_shutdown("Tracker reset to origin.")
--- scripts/vive_world.py
+++ scripts/vive_world.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
@@ -7,36 +7,40 @@ import time
 import sys
 import tf
 
+
 def vive_world():
-    rospy.init_node('vive_tracker_frame')
+    rospy.init_node("vive_tracker_frame")
     listener = tf.TransformListener()
-    rate = rospy.Rate(100) # 10hz
-    translation = [0,0,0]
-    rotation = [0.707,0.0,0.0,0.707]
+    rate = rospy.Rate(100)  # 10hz
+    translation = [0, 0, 0]
+    rotation = [0.707, 0.0, 0.0, 0.707]
     broadcaster = tf.TransformBroadcaster()
-    broadcaster.sendTransform(translation,
-                    rotation,
-                    rospy.Time.now(),
-                    'vive_world',
-                    'map')
-    sys.stdout.write('Generating vive_world frame.')
+    broadcaster.sendTransform(
+        translation, rotation, rospy.Time.now(), "vive_world", "map"
+    )
+    sys.stdout.write("Generating vive_world frame.")
     frame_generated = False
     while not rospy.is_shutdown():
         try:
-            (translation, rotation) = listener.lookupTransform('map', 'vive_world', rospy.Time(0))
+            (translation, rotation) = listener.lookupTransform(
+                "map", "vive_world", rospy.Time(0)
+            )
             if frame_generated is False:
-                print ""
+                print("")
                 frame_generated = True
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            sys.stdout.write('.') 
-        broadcaster.sendTransform(translation,
-                        rotation,
-                        rospy.Time.now(),
-                        "vive_world",
-                        "map")
+        except (
+            tf.LookupException,
+            tf.ConnectivityException,
+            tf.ExtrapolationException,
+        ):
+            sys.stdout.write(".")
+        broadcaster.sendTransform(
+            translation, rotation, rospy.Time.now(), "vive_world", "map"
+        )
         rate.sleep()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         vive_world()
     except rospy.ROSInterruptException:
